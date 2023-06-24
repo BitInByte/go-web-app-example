5f23bb16b3c183b11bc3794a1d1e2277e7fba0cd
BitInByte <bitinbyte@outlook.com>
2023-06-24 03:20:00 +0100

    Add: http request validation

--- http/app-request.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-httpie localhost:8000
---
+++ http/login-request.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+http -vv --session=./session.json POST localhost:3000/v1/auth/login email=test@test.com username=test password=test123
